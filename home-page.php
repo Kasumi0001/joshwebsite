0985eaa7b100e9126f7302faacbf2051c7593e78
Kasumi0001 <iGamers.Kasumi0001@gmail.com>
2024-03-29 22:08:44 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,47 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+<section class="container-fluid aboutbg text-center">
+    <div class="container">
+
+        <div class="blocker"></div>
+    </div>
+</section> <!-- container-fluid -->
+
+<section class="container-fluid articlesbg">
+    <div class="container">
+<h3 class="text-center">LATEST ARTICLES</h3>
+<div class="row">
+
+<!-- 3 ARTICLES WITH IMAGES -->
+    <div class="col-md-4">
+        <img class="img-fluid" src="http://170.187.231.66/~mesh20/sub2/wp-content/themes/joshwebsite/images/woman.jpg" alt="woman">
+        <h4 class="article-title">A PORTRAIT</h4>
+        <p class="date">Friday November 2030</p>
+        <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
+        </p>
+        <a class="read-more" href="#">CONTINUE READING</a>     
+    </div>
+    <div class="col-md-4">
+        <img class="img-fluid" src="http://170.187.231.66/~mesh20/sub2/wp-content/themes/joshwebsite/images/robot.jpg" alt="robot">
+        <h4 class="article-title">A PORTRAIT</h4>
+        <p class="date">Friday November 2030</p>
+        <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
+        </p>
+        <a class="read-more" href="#">CONTINUE READING</a>     
+    </div>
+    <div class="col-md-4">
+        <img class="img-fluid" src="http://170.187.231.66/~mesh20/sub2/wp-content/themes/joshwebsite/images/mapping.jpg" alt="mapping">
+        <h4 class="article-title">A PORTRAIT</h4>
+        <p class="date">Friday November 2030</p>
+        <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
+        </p>
+        <a class="read-more" href="#">CONTINUE READING</a>     
+    </div>
+    </div> <!-- row -->
+</div> <!-- container -->
+</section> <!-- container-fluid -->
+<?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
